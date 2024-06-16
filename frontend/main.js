c6507723eb7d11631a2d6e87c04d6b457cccd5e9
“Anuj <anujchoudhari26@gmail.com>
2024-06-16 16:14:54 +0530

    updated functionAPI

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded',(event) => {
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApi = 'https://getresumecounteranuj.azurewebsites.net/api/GetResumeCounter?code=e8zrV_jgoHcvL6kNUEWu-eLonQM57ztV84A0zXEFznA8AzFuX9T3Mg%3D%3D';
 
 const getVisitCount = () => {
     let count =30;
